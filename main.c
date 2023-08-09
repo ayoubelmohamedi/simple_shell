59c7c4666e0a5d5baa48390e5f930ffd716a3419
RevolverSnake412 <oussag@hotmail.fr>
2023-08-09 18:49:56 +0100

    Task 1 done

--- main.c
+++ main.c
@@ -1,20 +1,40 @@
 #include "main.h"
 #include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 
 /**
  * main - main
  *
  * Return: 0 on success
 */
-int main()
+int main(void)
 {
 	char *prompt = retrieve_prompt();
-	char user_input[100];
+	char command[128];
+	char *argv[2];
+	char *envv[1] = {NULL};
+	int status;
+	pid_t pid;
 
-	while(1)
+	while (1)
 	{
 		printf("%s ", prompt);
-		scanf("%s", user_input);
+		scanf("%s", command);
+
+		argv[0] = command;
+		argv[1] = NULL;
+
+		pid = fork();
+		waitpid(pid, &status, 0);
+
+		if (pid == 0)
+		{
+			if (execve(command, argv, envv) == -1)
+				perror("Error");
+		}
 	}
 
 	return (0);
