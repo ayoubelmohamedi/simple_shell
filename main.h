1da7d6fdd4048f5d546ef1991da6cd9beeefdd5e
ayoubelmohamedi <ayoubprog61@gmail.com>
2023-08-14 07:28:56 -0700

    add BUFFSIZE macro to main.h

--- main.h
+++ main.h
@@ -1,6 +1,12 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+/**##### MACROS ######*/
+
+#define BUFSIZE 1024
+
+/**###### LIBS USED ######*/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -8,6 +14,7 @@
 #include <pwd.h>
 #include <sys/types.h>
 #include <sys/wait.h>
+
 
 char *retrieve_prompt();
 void fork_process(char *cmd, char **args);
